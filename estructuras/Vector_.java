1aa0fa5e86233c8f60a1a69ade4288a3ddd825ea
Diego Diaz moron <alu0101337975@ull.edu.es>
2022-03-19 15:17:21 +0000

    Hecho hasta el apartado 4

--- estructuras/Vector_.java
+++ estructuras/Vector_.java
@@ -1,30 +1,47 @@
 package estructuras;
 
+import java.util.Scanner;
+
 public class Vector_ {
 	double[] doubleArray;
 	int size;
 	public Vector_(int size) {
 		this.size = size;
 		doubleArray = new double[size];
+		double d1;
+		Scanner sc = new Scanner(System.in); 
+		for(int i=0;i<size;i++) {				
+			System.out.print("Introduce el valor para la posición " + i + ": ");
+			d1 = sc.nextDouble(); 
+			this.setValor(d1, i);
+		}
 	}
 	public void setValor (double valor, int pos) {
-		// Anadir excepcion
-		doubleArray[pos] = valor;
+		try {
+			doubleArray[pos] = valor;			
+		} catch(IndexOutOfBoundsException e){
+			System.err.println("IndexOutOfBoundsException:" + e.getMessage());
+		}
 	}
 	// Devuelve las dimensiones del vector
 	public int tamano() {
 		return size;
 	}
-	//  escribe por pantalla los coeficientes del vector
+	//  Escribe por pantalla los coeficientes del vector
 	public void escribir() {
 		for(int i=0;i<size;i++) System.out.print(doubleArray[i] + " ");
 		System.out.println();
 	}
 	// Producto escalar
 	public double productoEscalar(Vector_ v2) {
-		// Anadir excepcion
 		double output = 0;
-		for(int i=0;i<size;i++) output += doubleArray[i] * v2.doubleArray[i];
+		try {			
+			for(int i=0;i<size;i++) output += doubleArray[i] * v2.doubleArray[i];
+		}
+		catch(IndexOutOfBoundsException e){
+			System.err.println("IndexOutOfBoundsException: No se puede hacer el producto escalar de dos vectores de diferentes tamaños");
+			return 0.0;
+		}
 		return output;
 	}
 }
--- paquete1/Ejercicio1.java
+++ paquete1/Ejercicio1.java
@@ -11,13 +11,10 @@ public class Ejercicio1 {
 	private static Scanner sc = new Scanner(System.in); 
 	
 	public static void main(String[] args) {
-		Ejercicio1.apartado1();
-		Ejercicio1.apartado2();
-		Ejercicio1.apartado3();
+		//Ejercicio1.apartado1();
+		//Ejercicio1.apartado2();
+		//Ejercicio1.apartado3();
 		Ejercicio1.apartado4();
-		/*
-		
-		*/
 	}
 	public static void apartado1() {
 		System.out.println("Primos:");
@@ -51,18 +48,19 @@ public class Ejercicio1 {
 	}
 	public static void apartado4() {
 		System.out.println("Vectores:");
-		Vector_ v1 = new Vector_(2);
-		v1.setValor(1.5, 0);
-		v1.setValor(2, 1);
-		System.out.print("v1: ");
+		System.out.println("Introduce tamaño de v1:");
+		int n1 = sc.nextInt();
+		Vector_ v1 = new Vector_(n1);
+		System.out.println("Introduce tamaño de v2:");
+		n1 = sc.nextInt();
+		Vector_ v2 = new Vector_(n1);
+		System.out.println("v1: ");
 		v1.escribir();
-		Vector_ v2 = new Vector_(2);
-		v2.setValor(3, 0);
-		v2.setValor(4.5, 1);
-		System.out.print("v2: ");
-		v2.escribir();
-		double prod_esc = v1.productoEscalar(v2);
+		System.out.println("v2: ");
+		v2.escribir();		
 		System.out.print("Producto Escalar: ");
+		double prod_esc = v1.productoEscalar(v2);
 		System.out.println(prod_esc);
+		
 	}
 }
