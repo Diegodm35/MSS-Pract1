cd2674a27c3ead447a6737865a8f0efa26643d88
Diego Diaz moron <alu0101337975@ull.edu.es>
2022-03-20 00:57:18 +0000

    Finalizado

--- estructuras/Vector_.java
+++ estructuras/Vector_.java
@@ -10,32 +10,49 @@ public class Vector_ {
 	public Vector_(int size) {
 		this.size = size;
 		doubleArray = new double[size];
+		for(int i=0;i<size;i++) this.setVal(0, i);
+	}
+	
+	public Vector_() {
+		this(3);
+	}
+	// Devuelve las dimensiones del vector
+	public int getSize() {
+		return size;
+	}
+	
+	public double[] getVec() {
+		return doubleArray;
+	}
+	
+	public double getVal(int pos) {
+		return doubleArray[pos];
+	}
+	
+	public void setVal (double valor, int pos) {
+		doubleArray[pos] = valor;			
+	}
+	
+	public void setVec(double[] vec) {
+		doubleArray = vec;
+	}
+	// Establece todos los valores del vector por pantalla
+	public void setAllVals() {
 		double d1;
 		Scanner sc = new Scanner(System.in); 
 		for(int i=0;i<size;i++) {				
-			System.out.print("Introduce el valor para la posición " + i + ": ");
+			System.out.print("Posición " + i + ": ");
 			d1 = sc.nextDouble(); 
-			this.setValor(d1, i);
+			this.setVal(d1, i);
 		}
-	}
-	public void setValor (double valor, int pos) {
-		try {
-			doubleArray[pos] = valor;			
-		} catch(IndexOutOfBoundsException e){
-			System.err.println("IndexOutOfBoundsException:" + e.getMessage());
-		}
-	}
-	// Devuelve las dimensiones del vector
-	public int tamano() {
-		return size;
 	}
 	//  Escribe por pantalla los coeficientes del vector
-	public void escribir() {
+	public void write() {
 		for(int i=0;i<size;i++) System.out.print(doubleArray[i] + " ");
 		System.out.println();
 	}
 	// Pasa al string el vector
-	public String to_s() {
+	public String toString() {
 		String output = "";
 		for(int i=0;i<size;i++) {
 			output += doubleArray[i] + ",";
@@ -43,25 +60,15 @@ public class Vector_ {
 		return output;
 	}
 	// Producto escalar
-	public double productoEscalar(Vector_ v2) {
+	public double scalarProd(Vector_ v2) {
 		double output = 0;
-		try {			
-			for(int i=0;i<size;i++) output += doubleArray[i] * v2.doubleArray[i];
-		}
-		catch(IndexOutOfBoundsException e){
-			System.err.println("IndexOutOfBoundsException: No se puede hacer el producto escalar de dos vectores de diferentes tamaños");
-			return 0.0;
-		}
+		for(int i=0;i<size;i++) output += doubleArray[i] * v2.doubleArray[i];
 		return output;
 	}
 	// Escribir en fichero
-	public void file_write(String name) {
-		 try {
-			FileWriter fw=new FileWriter(name);
-			fw.write(this.to_s());    
-			fw.close(); 
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	public void writeFile(String name) throws IOException {
+		FileWriter fw=new FileWriter(name);
+		fw.write(this.toString());    
+		fw.close(); 
 	}
 }
